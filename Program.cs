13ae3f89843aecd0791a43eea9d4796c98c13eb3
Aluno <Aluno@DESKTOP-EGCADHE>
2024-09-12 21:42:10 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace nomesIdades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos Nomes/Idades deseja cadastrar: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            string[] nomes = new string[quantidade];
+            int[] idades = new int[quantidade];
+
+
+            for (int i = 0; i < quantidade.Length; i++)
+            {
+                Console.Write("Escreva o nome: ");
+                nomes[i] = Console.ReadLine();
+
+                Console.Write("Escreva a idade: ");
+                nomes[i] = Console.ReadLine();
+            }
+
+            for (int i = 0; i < quantidade; i++)
+            {
+                Console.WriteLine($ "Nome: {nomes[i]}, Idade: {idades[i]}");
+            }
+        }             
+    }
+}
